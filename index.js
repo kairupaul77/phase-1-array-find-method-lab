114405805fcd87a0af52d2b72aa3b6513f05417f
paul kairu <paulkairukamau@gmail.com>
2024-10-12 22:39:47 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    // Use find() to locate the first object where result is "W"
+    const winningYear = arrayOfObjects.find(function(obj) {
+        return obj.result === "W";
+    });
+    
+    // If we found a winning year, return the year, otherwise return undefined
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        return undefined;
+    }
+}
